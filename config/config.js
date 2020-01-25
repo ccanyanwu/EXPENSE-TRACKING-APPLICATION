de2ea17fb0afd49db5713586bbe02e3d3248f679
Sarah-MB <Sarahembee71@gmail.com>
2020-01-25 18:50:01 +0000

    Starter kit for node application

---
+++ config/config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  development: {
+    dialect: "sqlite",
+    storage: "./db.development.sqlite"
+  },
+  test: {
+    dialect: "sqlite",
+    storage: ":memory:"
+  },
+  dev: {
+    username: "manifestUser",
+    password: "manifestPassword",
+    database: "postgres",
+    host: "manifestdbinstance.cgq0reqixqsd.us-east-1.rds.amazonaws.com",
+    dialect: 'postgres' 
+  },
+  production: {
+    username: "manifestUser",
+    password: "manifestPassword",
+    database: "postgres",
+    host: "manifestdbinstance.cgq0reqixqsd.us-east-1.rds.amazonaws.com",
+    dialect: 'postgres' 
+  }
+  // ,
+  // production: {
+  //   username: process.env.DB_USERNAME,
+  //   password: process.env.DB_PASSWORD,
+  //   database: process.env.DB_NAME,
+  //   host: process.env.DB_HOSTNAME,
+  //   dialect: 'mysql',
+  //   use_env_variable: 'DATABASE_URL'
+  // }
+};
