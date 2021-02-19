29f86c9e96024d4edf61ea89891a55e10bfab0d5
ccanyanwu <eme42c@gmail.com>
2021-02-19 10:19:53 +0100

    modified employee model

--- models/employee.js
+++ models/employee.js
@@ -5,49 +5,20 @@ const AccessControl = require('accesscontrol');
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   var Employee = sequelize.define('Employee', {
-    first_name: { 
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-        notEmpty: true,
-        } 
-    },
-    last_name: { 
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-        notEmpty: true,
-        } 
-    },
-    username: { 
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-        notEmpty: true,
-        } 
-    },
-    email: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-          isEmail: true,
-    }},
+    first_name: {type: DataTypes.STRING},
+    last_name: {type: DataTypes.STRING},
+    username: {type: DataTypes.STRING},
+    email: {type: DataTypes.STRING},
     password: {
-    type: Sequelize.STRING,
+    type: DataTypes.STRING/*,
     validate: {
         len: { 
             args: [7, 42],
             msg: "The password length should be between 7 and 18 characters."
         }
     },
-},
-    mobile:{
-        type: DataTypes.BIGINT,
-        allowNull: false,
-        validate: {
-            isNumeric : true
-        }
-    },
+  */},
+    mobile:{type: DataTypes.BIGINT},
     role: { 
       type: DataTypes.STRING,
       allowNull: false,
