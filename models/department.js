2834f0a45298633708e758e6a99bbb39d664021d
ccanyanwu <eme42c@gmail.com>
2021-02-26 12:57:02 +0100

    corrected error in model comments

--- models/department.js
+++ models/department.js
@@ -11,12 +11,11 @@ module.exports = (sequelize, DataTypes) => {
     }
   });
   
-//create association between department, expens and employee
+//create association between department, expense and employee
   // a department can have more than 1 employee and expense
   Department.associate = (models) => {
     models.Department.hasMany(models.Employee);
     models.Department.hasMany(models.Expense);
-
   };
 
   return Department;
